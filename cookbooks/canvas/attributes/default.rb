2034cf3e0b1c0a8b9271f76c52aa076481f0423e
Neal Lawson <nlawson@purpleboxnetworks.com>
2012-09-16 09:11:37 -0700

    some cleanup

--- cookbooks/canvas/attributes/default.rb
+++ cookbooks/canvas/attributes/default.rb
@@ -1,23 +1,25 @@
+#System user to create and use for canvas
 default["canvas"]["system_user"]  = "canvas"
 default["canvas"]["system_group"] = "canvas"
 default["canvas"]["system_uid"]   = 400
 default["canvas"]["system_gid"]   = 400
 default["canvas"]["home_dir"]     = "/opt/canvas"
+
+#GIT Repo Settings
 default["canvas"]["git_repo"]     = "https://github.com/instructure/canvas-lms.git"
 default["canvas"]["git_branch"]   = "stable"
+
+#SQL Server Settings
 default["canvas"]["sql_user"]     = "setme"
 default["canvas"]["sql_password"] = "setme"
 default["canvas"]["sql_server"]   = "setme"
 default["canvas"]["sql_db_name"]  = "canvas_production"
-default["canvas"]["sql_queue_db_name"] = "canvas_queue_production"
-default["canvas"]["sql_db_timeout"] = 5000
-default["canvas"]["sql_db_driver"] = "postgresql"
-default["canvas"]["sql_db_create_db"] = false
-default["canvas"]["redis_server"] = "setme"
-default["canvas"]["fqdn"] = "canvas.local"
-default["canvas"]["files_fqdn"] = "canvas.local"
-default["canvas"]["random_key"] = "you-really-need-to-set-me"
-default["canvas"]["file_store_location"] = "/opt/canvas/file_store"
+default["canvas"]["sql_queue_db_name"]  = "canvas_queue_production"
+default["canvas"]["sql_db_timeout"]     = 5000
+default["canvas"]["sql_db_driver"]      = "postgresql"
+default["canvas"]["sql_db_create_db"]   = false
+
+#Outoging Mail Setttings
 default["canvas"]["use_sendmail"] = true
 default["canvas"]["outgoing_mail_server"] = "smtp.somedomain.com"
 default["canvas"]["outgoing_mail_server_port"] = 25
@@ -27,7 +29,12 @@ default["canvas"]["outgoing_mail_server_auth_method"] = "none" # can be: plain, 
 default["canvas"]["outgoing_mail_server_domain"] = "example.com"
 default["canvas"]["outgoing_mail_server_email_address"] = "canvas@example.com"
 default["canvas"]["outgoing_mail_server_email_name"] = "The Learnistute"
+
+#General Canvas Settings
+default["canvas"]["redis_server"]       = "setme"
+default["canvas"]["fqdn"] = "canvas.local"
+default["canvas"]["random_key"] = "you-really-need-to-set-me"
+default["canvas"]["file_store_location"] = "/opt/canvas/lms/file_store"
 default["canvas"]["apache_ssl_key"] = "/etc/ssl/private/ssl-cert-snakeoil.key"
 default["canvas"]["apache_ssl_cert"] = "/etc/ssl/certs/ssl-cert-snakeoil.pem"
 default["canvas"]["apache_ssl_chain"] = "NOCA" # use "NOCA" if you do not have an SSL Certificate Chain File
-default["canvas"][""]
--- cookbooks/canvas/metadata.rb
+++ cookbooks/canvas/metadata.rb
@@ -1,5 +1,5 @@
-maintainer       "YOUR_COMPANY_NAME"
-maintainer_email "YOUR_EMAIL"
+maintainer       "Neal Lawson"
+maintainer_email "neal.lawson@gmail.com"
 license          "All rights reserved"
 description      "Installs/Configures canvas"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
