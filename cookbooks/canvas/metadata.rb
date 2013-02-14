351950a71ccc58d398abcd21ce20cf9670a3a378
Neal Lawson <nlawson@purpleboxnetworks.com>
2013-02-14 15:30:05 -0800

    adding nginx dep

--- cookbooks/canvas/metadata.rb
+++ cookbooks/canvas/metadata.rb
@@ -4,3 +4,5 @@ license          "All rights reserved"
 description      "Installs/Configures canvas"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.2"
+
+depends 'nginx', '>= 1.3.0'
